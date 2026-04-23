7664b55016290aed6546cc198c446198bead4469
atama-gapon <ktq2590352@stu.o-hara.ac.jp>
2026-04-23 09:44:41 +0900

    Merge branch 'main' of https://github.com/atama-gapon/score-manager-repository.git into main

--- src/main/java/scoremanager/main/SubjectListAction.java
+++ src/main/java/scoremanager/main/SubjectListAction.java
@@ -14,22 +14,14 @@ import tool.Action;
 public class SubjectListAction extends Action {
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 // 【セッションからユーザーデータを取得】
-		// 【テスト環境の処理】
-		//School school = new School();
-		//school.setCd("oom");
-		//school.setName("テスト：oom");
-		// 【/テスト環境の処理】
-		
-		// 【本番環境の処理】
-		 HttpSession session = req.getSession();
-		 Teacher teacher = (Teacher)session.getAttribute("user");
-		 School school = teacher.getSchool();
-		// 【/本番環境の処理】
-
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+		School school = teacher.getSchool();
+		 
 // 【セッションのユーザーデータから、ユーザーが所属している学校の科目一覧用データを取得】
-// 【学校コードに合致する科目の一覧を取得】
 		SubjectDao sDao = new SubjectDao();
 		List<Subject> subjectSet = sDao.filter(school);
+
 		req.setAttribute("subjects", subjectSet);
 		req.getRequestDispatcher("subject_list.jsp").forward(req, res);
 	}
